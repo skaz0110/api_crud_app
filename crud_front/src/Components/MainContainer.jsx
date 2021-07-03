c86a0de9b17351381f660a1417d387779f977f28
kazu <skaz0110@yahoo.co.jp>
2021-07-03 17:16:40 +0900

    create機能の追加

--- crud_front/src/Components/MainContainer.jsx
+++ crud_front/src/Components/MainContainer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import axios from 'axios'
 import ProductsContainer from './ProductsContainer'
+import FormContainer from './FormContainer'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import update from 'react-addons-update'
 
 
 
@@ -25,11 +29,23 @@ componentDidMount() {
     })
   }
 
+  createProduct = (product) => {
+    axios.post('http://localhost:3001/products',{product: product} )
+    .then((response) => {
+      const newData = update(this.state.products, {$push:[response.data]})
+      this.setState({products: newData})
+    })
+    .catch((data) =>{
+      console.log(data)
+    })
+  }
+  
   render() {
     return (
       <div className='app-main'>
+        <FormContainer createProduct={this.createProduct}/>
         <ProductsContainer productData={ this.state.products } />
-      </div>
+    </div>
     );
   }
 }
